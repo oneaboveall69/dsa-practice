74b11474ebfdf236b13303280ed7e5d37567545f
Prince Abraham <princeabraham619@gmail.com>
2025-08-03 20:12:56 +0530

    Create  two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+        for i, num in enumerate(nums):
+            comp = target - num
+            if comp in num_map:
+                return [num_map[comp], i]
+            num_map[num] = i
